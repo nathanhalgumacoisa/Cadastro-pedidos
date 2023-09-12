c13cedca4cb22f6ccd7b9bc4be26132e129b64a8
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-09-12 15:12:06 -0300

    new feat: function mostrarPedido

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,8 +22,11 @@ class ListaPedido{
     listarPorId(id){
         return this.pedidos.find((pedido) => pedido.id == id);
     }
-    atualizarPorId(){
-
+    atualizarPorId(id, cliente, mesa, descricao){
+        let pedido = this.listarPorId(id);
+        pedido.cliente = cliente;
+        pedido.mesa = mesa
+        pedido.descricao = descricao
     }
     deletarPorId(){
 
@@ -38,9 +41,29 @@ function criarPedido(){
     pedidos.adicionar(pedido);
     //console.log(pedidos)
     limparInputs()
+    mostrarPedido()
 }
 function limparInputs(){
     document.getElementById("cliente").value = ""
     document.getElementById("mesa").value = ""
     document.getElementById("descricao").value = ""
+}
+function mostrarPedido(){
+    const pedidosArray = pedidos.listar();
+    let html = ""
+    pedidosArray.forEach((pedido)=> {
+        html += `
+            <div id="oPedido">
+                <p>Id: ${pedido.id}</p>
+                <p>Cliente: ${pedido.cliente}</p>
+                <p>Mesa: ${pedido.mesa}</p>
+                <p>Descrição ${pedido.descricao}</p>
+            </div>
+            <div id="btns">
+            <button onclick="deletarPedido"><i class="fa-sharp fa-solid fa-trash"></i></button>
+            <button onclick="editarPedido"><i class="fa-solid fa-pen"></i></button>
+            </div>
+        `
+    })
+    document.getElementById("listaPedido").innerHTML = html;
 }
